90ac6dcb18f8cccdb51cfcb40a1ce936df49b468
ForrestS2017 <fsmith2017@gmail.com>
2020-04-16 19:15:44 -0400

    Data Controller Skeleton
    + Bare bones Serializer skeleton. Will update after determening to save via serialization or JSON

---
+++ src/util/DataController.java
@@ -0,0 +1,41 @@
+package util;
+
+import model.Course;
+import model.User;
+
+import java.util.ArrayList;
+
+public class DataController {
+    private String path = "data.txt";
+    private ArrayList<Course> courses;
+    private ArrayList<User> users;
+
+    /**
+     * Update all save data with new data from session
+     */
+    public static boolean saveAll() {
+        return false;
+    }
+
+    /**
+     * Update course in save data
+     */
+    public static boolean saveCourse() {
+        return false;
+    }
+
+    /**
+     * Update user in save data
+     */
+    public static boolean saveUser() {
+        return false;
+    }
+
+    /**
+     * Load saved data
+     */
+    public static boolean read() {
+        return false;
+    }
+
+}
